34be6dd3c6a5b63bb7b9a042113ad17f8cb6a0cf
beinjamin <jauresbeinjamin@gmail.com>
2021-12-08 19:58:22 +0100

    Gestion de cas figure des deux direction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, FromsModule } from '@angular/forms';
+import { FormsModule   } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
